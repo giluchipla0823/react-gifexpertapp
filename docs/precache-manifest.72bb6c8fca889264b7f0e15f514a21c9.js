7a70b3db3c1c92ead86fdc4e07956513fcc13999
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2020-07-18 13:26:16 +0200

    Docs agregado

---
+++ docs/precache-manifest.72bb6c8fca889264b7f0e15f514a21c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bd0d143182c7a63abb51c2304a29ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5eabdfa8cb9256ed582",
+    "url": "/static/css/main.2fe2b618.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5eabdfa8cb9256ed582",
+    "url": "/static/js/main.db05dcfd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
